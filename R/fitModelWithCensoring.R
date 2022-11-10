9f90f2a15fc457900f55daf173e0401b976e5c12
GidiusVanDeKamp <gidius1996@hotmail.com>
2022-11-10 16:19:49 +0100

    lets see if this fit is better

--- R/fitModelWithCensoring.R
+++ R/fitModelWithCensoring.R
@@ -15,17 +15,17 @@ fitModelWithCensoring <- function(Trainingset,  #do now Trainingset$Train
                                                 modelSettings = modelSettings,
                                                 analysisId = "outcome_model")
 
-  censoringPop <- Trainingset
-  censoringPop$labels <- censoringPop$labels %>%
-    dplyr::mutate(
-      outcomeCount = as.numeric(!(as.logical(.data$outcomeCount)))
-    )
+  # censoringPop <- Trainingset
+  # censoringPop$labels <- censoringPop$labels %>%
+  #   dplyr::mutate(
+  #     outcomeCount = as.numeric(!(as.logical(.data$outcomeCount)))
+  #   )
 
-# censoringPop$labels <- censoringPop$labels %>%
-#   dplyr::mutate(
-#     survivalTime = timeAtRisk,
-#     outcomeCount = (timeAtRisk != 7300)*1  # make this mode general this is the enddate.
-#   )
+censoringPop$labels <- censoringPop$labels %>%
+  dplyr::mutate(
+    survivalTime = timeAtRisk,
+    outcomeCount = (timeAtRisk != 7300)*1  # make this mode general this is the enddate.
+  )
 
 
   fitCensoring <- PatientLevelPrediction::fitPlp(trainData = censoringPop,
